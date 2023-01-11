8f39b7e9f4062fb97c523de1b796f7534e6c93a0
tong.yao <tong.yao@check24.de>
2023-01-11 08:28:20 +0100

    initial setup of the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import Header from './componnet/header/Header';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Header />
       <div className="App">
         <h1>Setup React Redux</h1>
       </div>
